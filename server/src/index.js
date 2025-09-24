5a2e466cdd4ebffc8d2cfdc2d1226e1b922bdf55
Hyun <eric0909kim@gmail.com>
2025-09-24 09:57:00 -0400

    update store service path for local assets and index.js to display static assets

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -7,6 +8,8 @@ const routes = require('./routes/routes.js');
 
 app.use(express.json());
 app.use('/', routes);
+
+app.use('/snapshots', express.static(path.join(__dirname, '../snapshots')));
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port ${process.env.PORT}`);
--- server/src/services/storeService.js
+++ server/src/services/storeService.js
@@ -2,11 +2,38 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const { URL } = require('url');
+const cheerio = require('cheerio');
 
 async function saveHTML(data, url, storePath) {
     const newUrl = new URL(url);
     const newPath = path.join(storePath, newUrl.hostname); // store html under path: original path + hostname
     fs.mkdirSync(newPath, { recursive: true });
+
+    const $ = cheerio.load(data);
+
+    $('[src], [href]').each((index, element) => {
+        // Determine which attribute to update: 'src' or 'href'
+        const attrib = $(element).attr('src') ? 'src' : 'href';
+        let link = $(element).attr(attrib);
+        if (!link) return;
+
+        try {
+            const absUrl = new URL(link, url);
+            if (absUrl.hostname === newUrl.hostname) {
+                // Construct new relative path to the saved asset in snapshots folder
+                const relativePath = path.posix.join(
+                    '/snapshots',
+                    newUrl.hostname,
+                    path.basename(storePath),
+                    absUrl.hostname,
+                    absUrl.pathname
+                );
+                $(element).attr(attrib, relativePath); // Replace the element's src/href attribute with the new relative path
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    });
 
     let cleanedFile = newUrl.pathname.replace(/[\/:\\?%*|"<>]/g, '_'); // clean unwanted symbols like * or |
     if (!cleanedFile || cleanedFile == "_") {
@@ -14,7 +41,7 @@ async function saveHTML(data, url, storePath) {
     }
     const fileName = cleanedFile + '.html';
     const filePath = path.join(newPath, fileName); // final full path cleaned and updated
-    fs.writeFileSync(filePath, data, 'utf-8');
+    fs.writeFileSync(filePath, $.html(), 'utf-8');
 }
 
 async function saveAsset(link, storePath) {
