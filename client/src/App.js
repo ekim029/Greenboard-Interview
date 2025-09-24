b939219013b3e17a3c4bc5e02085aa8ff145bf7b
Hyun <eric0909kim@gmail.com>
2025-09-24 15:14:22 -0400

    update app.js to handle submit form

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
+import NewArchive from './components/newArchive';
+import { useState } from 'react';
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (url) => {
+    setLoading(true);
+    setSuccess(false);
+    setError(null);
+
+    try {
+      const res = await fetch('http://localhost:3000/submit', {
+        method: "POST",
+        body: JSON.stringify({ url }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+
+      if (!res.ok) {
+        throw new Error('');
+      }
+      setSuccess(true);
+
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="App">
+      <h1>Website Archiving</h1>
+      <NewArchive onSubmit={handleSubmit} />
 
+      {loading && <p>Archiving URL...</p>}
+      {success && <p>Successfully Archived</p>}
+      {error && <p>{error}</p>}
     </div>
   );
 }
