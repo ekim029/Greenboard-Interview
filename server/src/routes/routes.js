99cd4e61797ce81a82deb6a359870ab4d631b1e1
Hyun <eric0909kim@gmail.com>
2025-09-24 10:38:10 -0400

    Update routes to remove archive by timestamp. (Will be done with filtering in react)

--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -2,10 +2,8 @@ const express = require('express');
 const router = express.Router();
 const submit = require('../controllers/submitController.js');
 const listArchived = require('../controllers/listArchivedController.js');
-const listArchivedByTimestamp = require('../controllers/listArchivedByTimestampController.js');
 
 router.post('/submit', submit);
 router.get('/archived/:site', listArchived);
-router.get('/archived/:site/:timestamp', listArchivedByTimestamp);
 
 module.exports = router;
