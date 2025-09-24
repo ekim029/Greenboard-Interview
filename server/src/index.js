973725639abcf5110f60e1a5ad868144a2586793
Hyun <eric0909kim@gmail.com>
2025-09-24 15:08:37 -0400

    Update backend index.js to use cors to allow frontend interaction

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 
 const path = require('path');
+const cors = require('cors');
 const express = require('express');
 const app = express();
 
 const routes = require('./routes/routes.js');
 
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
